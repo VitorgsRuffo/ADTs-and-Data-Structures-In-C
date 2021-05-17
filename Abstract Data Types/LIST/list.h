325f3c3729b795efd06fa7c92fef4b08566bd16c
VitorgsRuffo <vitorruffo19cc@gmail.com>
2021-05-17 19:44:14 -0300

    LIST ADT: adding operations time complexity.

--- Abstract Data Types/LIST/list.h
+++ Abstract Data Types/LIST/list.h
@@ -33,26 +33,26 @@ typedef void* List;
 
 
 /*
-* Instantiates a new variable of type List. Time complexity: O(x).
+* Instantiates a new variable of type List. Time complexity: O(1).
 * @return New, empty list. Must be released with freeList()
 */
 List newList(void);
 
 /*
-* @param Li List object. Time complexity: O(x).
+* @param Li List object. Time complexity: O(1).
 * @return Number of elements in the list.
 */
 int listSize(List Li);
 
 /*
-* Checks if list is empty. Time complexity: O(x).
+* Checks if list is empty. Time complexity: O(1).
 * @param Li List object.
 * @return 1 if the list is empty, otherwise, 0.
 */
 int isListEmpty(List Li);
 
 /*
-* Adds an element to the end of the list. Time complexity: O(x).
+* Adds an element to the end of the list. Time complexity: O(n).
 * @param Li List object.
 * @param data Data object.
 * @return 1 if the element was successfully added to the list, otherwise, 0.
@@ -60,7 +60,7 @@ int isListEmpty(List Li);
 int listAdd(List Li, Data data);
 
 /*
-* Adds an element at the specified position in the list. Time complexity: O(x).
+* Adds an element at the specified position in the list. Time complexity: O(n).
 * @param Li List object.
 * @param position List position in which the element is going to be added.
 * @param data Data object (or, element).
@@ -69,7 +69,7 @@ int listAdd(List Li, Data data);
 int listAddAt(List Li, int position, Data data);
 
 /* 
-* Time complexity: O(x).
+* Time complexity: O(n) for linked list implementation, O(1) for array implementatino.
 * @param Li List object.
 * @param position List position of an element.
 * @return Element at the specified position in the list. If position is invalid NULL is returned.
@@ -77,7 +77,7 @@ int listAddAt(List Li, int position, Data data);
 Data listGet(List Li, int position);
 
 /*
-* Removes the element at the specified position in the list. Time complexity: O(x).
+* Removes the element at the specified position in the list. Time complexity: O(n).
 * @param Li List object.
 * @param position List position of an element.
 * @return Removed element. If position is invalid NULL is returned.
@@ -85,20 +85,20 @@ Data listGet(List Li, int position);
 Data listRemove(List Li, int position);
 
 /*
-* Reverses the list (e.g, [a b c d] becomes [d c b a]). Time complexity: O(x).
+* Reverses the list (e.g, [a b c d] becomes [d c b a]). Time complexity: O(n).
 * @param Li List object.
 */
 void listReverse(List Li);
 
 /*
-* Prints the list to the standard output file. Time complexity: O(x).
+* Prints the list to the standard output file. Time complexity: O(n).
 * @param Li List object.
 * @param function Function that returns the string representation of a list element.
 */
 void printList(List Li, dataToString function);
 
 /*
-* Desalocates all memory used by the list object. If function is passed, all elements in the list are also desalocated from memory. Time complexity: O(x).
+* Desalocates all memory used by the list object. If function is passed, all elements in the list are also desalocated from memory. Time complexity: O(n).
 * @param Li List object.
 * @param function Function that desalocates an element from memory.
 */
